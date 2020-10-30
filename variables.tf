2fa56759fb520ceaf1766c9447b46b094182e9fe
knutia <knutia@hotmail.com>
2020-10-30 10:13:42 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  default     = "pca-no-parram"
+  description = "Name for the resource group for all resources."
+}
+
+variable "resource_group_location" {
+  default     = "West Europe"
+  description = "Location for the resource group."
+}
