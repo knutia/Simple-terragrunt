0a38c192df832b0bebdbb6356bf7ab96942dbcc9
knutia <knutia@hotmail.com>
2020-12-04 09:10:08 +0100

    Update variables.tf

--- modules/azurerm_resource_group/variables.tf
+++ modules/azurerm_resource_group/variables.tf
@@ -1,9 +1,11 @@
 variable "resource_group_name" {
+  type        = string
   default     = "pca-no-parram"
   description = "Name for the resource group for all resources."
 }
 
 variable "resource_group_location" {
+  type        = string
   default     = "West Europe"
   description = "Location for the resource group."
 }
