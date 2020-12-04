9d5907af3e7dba7b0b450cbde20955b0cb316283
knutia <knutia@hotmail.com>
2020-12-04 09:07:34 +0100

    Update main.tf

--- modules/azurerm_resource_group/main.tf
+++ modules/azurerm_resource_group/main.tf
@@ -1,6 +1,6 @@
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
